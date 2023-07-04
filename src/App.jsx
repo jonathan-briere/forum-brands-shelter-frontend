216ccfef66a9d0068472fccdda4aed6f5a734854
jonathan-briere <jonathan.briere.dev@gmail.com>
2023-07-04 09:50:57 -0400

    Add Dashboard and Navigation Button

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Navbar } from "./components/Navbar";
 import "./App.css";
 
 import { Dashboard } from "./containers/Dashboard";
+import { routeUrls } from "./constants/routeUrls";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path={routeUrls.default} element={<Dashboard />} />
+          <Route path={routeUrls.dashboard} element={<Dashboard />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/containers/Dashboard/index.jsx
+++ src/containers/Dashboard/index.jsx
@@ -1,7 +1,58 @@
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Container,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import { CardView } from "components/CardView";
+import { DOG, CAT } from "constants/dashboardConstants";
+
 export const Dashboard = () => {
+  const [value, setValue] = useState();
+
+  console.log(value);
+
   return (
     <div>
-      <h1 className="best">Dashboard</h1>
+      <Typography
+        sx={{ m: "2rem", flexWrap: "wrap" }}
+        gutterBottom
+        variant="h2"
+        component="div"
+      >
+        Dashboard
+      </Typography>
+
+      <BottomNavigation
+        sx={{ width: 300, mx: "auto", mb: "1rem", boxShadow: 3, borderRadius: "0.5rem" }}
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Dogs" value={DOG} />
+        <BottomNavigationAction label="Cats" value={CAT} />
+      </BottomNavigation>
+
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "space-around",
+          flexWrap: "wrap",
+        }}
+      >
+        <CardView />
+        <CardView />
+        <CardView />
+        <CardView />
+        <CardView />
+        <CardView />
+        <CardView />
+        <CardView />
+        <CardView />
+      </Container>
     </div>
   );
 };
