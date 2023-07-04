4c186ec85f23f22d1e68d11c4a4b68a77a744d0c
jonathan-briere <jonathan.briere.dev@gmail.com>
2023-07-04 09:50:56 -0400

    Add Navbar and Addition Libraries

---
+++ src/components/CardView/index.jsx
@@ -0,0 +1,29 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+export const CardView = ({ object }) => (
+  <Card sx={{ minWidth: 250, m: "0.5rem", boxShadow: 2 }}>
+    <CardMedia
+      component="img"
+      height="140"
+      image={object?.image}
+      alt={object?.title}
+    />
+    <CardContent>
+      <Typography gutterBottom variant="h5" component="div">
+        {object?.title}
+      </Typography>
+      <Typography variant="body2" color="text.secondary">
+        {object?.description}
+      </Typography>
+    </CardContent>
+    {/* <CardActions>
+      <Button size="small">Share</Button>
+      <Button size="small">Learn More</Button>
+    </CardActions> */}
+  </Card>
+);
